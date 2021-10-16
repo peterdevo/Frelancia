d2bf8fd19b4c692ab82d086ee57a983fc9b39693
joke <joke@gmail.com>
2021-10-16 14:10:12 +0200

    Add Delete API

--- Server/Controllers/JobController.cs
+++ Server/Controllers/JobController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Application.JobPublishing;
@@ -14,12 +15,18 @@ namespace Server.Controllers
 
     public async Task<ActionResult<List<Job>>> GetJobs()
     {
-        return await Mediator.Send(new List.Query{});
+      return await Mediator.Send(new List.Query { });
     }
     [HttpPost]
     public async Task<IActionResult> CreateJob(Job job)
     {
       return Ok(await Mediator.Send(new Create.Command { Job = job }));
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteJob(Guid id)
+    {
+        return Ok( await Mediator.Send(new Delete.Command {Id=id}));
     }
   }
 }
